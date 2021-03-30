447e11a38e42545c584fee6f25b5659f16ee64f9
Amila Sampath <lucksy@gmail.com>
2021-03-31 00:30:30 +0200

    init files

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var del = require('del');
+var path = require('path');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+var sourcemaps = require('gulp-sourcemaps');
+var server = require('browser-sync').create();
+
+const { series } = require('gulp');
+
+const paths = {
+  scripts: {
+    src: './less/**/*.less',
+    dest: 'dist/'
+  }
+};
+
+const clean = () => del(['dist']);
+
+function scripts() {
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(less({
+        plugins: [autoprefix],
+        paths: [ path.join(__dirname, 'less') ]
+    }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './demo'
+    }
+  });
+  done();
+}
+
+const watch = () => gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+
+const dev = gulp.series(clean, scripts, serve, watch);
+
+exports.dev = dev;
+exports.default = series(dev);
+
